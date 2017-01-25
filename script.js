36f077f7b5ca344e537fc40e12d8ac66601bff06
saeed <sharifi.vsb@gmail.com>
2017-01-25 07:07:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+//////////////////////Get name///////////////////////////////////////////
+$(function() {
+	$.ajax({
+   		type: 'GET',
+   		url: 'http://gate.atlascon.cz:9999/rest/a/listNames',
+   		success: function(data) {
+   			for (var i = 0; i < data.length; i++) {
+     		$("#tbl2").append("<option>"+data[i]+"</option>");
+     		}
+   		},
+   	  	error: function() {
+        alert('error loading listNames!');
+    	},
+	});
+//////////////////////Get name space///////////////////////////////////////////
+	$('input#get').click(function(){
+		var middle = $('#tbl2 :selected').text(); 
+		$.ajax({
+			type: 'GET',
+			url:'http://gate.atlascon.cz:9999/rest/a/'+middle+'/namespace',
+			success:function(data2){
+				$('#text').html(data2);
+			},
+			error: function() {
+	        	alert('error loading Name Space!');
+	    	},
+		});
+	});
+//////////////////////////post(name and namespace)/////////////////////////////////////////////
+ 	$('#add-order').on('click', function() {
+	 	var name = $('#name').val();
+	 	var namespace = $('#drink').val();
+    	$.ajax({
+      		type: 'POST',
+      		url: 'http://gate.atlascon.cz:9999/rest/a/'+ name+"/"+namespace ,
+      		contentType: "application/json",
+      		success: function(newOrder) {
+			console.log("Friend added!", newOrder);
+      		},
+      		error: function() {
+	        	alert('error in Posting data to server!');
+	    	},
+    	});
+	});
+});
+
+
+
+
